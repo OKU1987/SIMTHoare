52ed39bcf3643b5d1ee8a54fbd98d84d5ffb8a39
OKU1987 <oku.87.s62.a4@gmail.com>
2015-01-09 15:14:06 +0900

    Define N

--- SIMT.v
+++ SIMT.v
@@ -1,2 +1,8 @@
 Require Import Vectors.VectorDef.
 Require Import ZArith.
+
+Section SIMT_Definition.
+  Variable num_threads : { n : nat & (n > 0)%nat}. (* the number of threads *)
+  Definition N := projT1 num_threads.
+
+End SIMT_Definition.
