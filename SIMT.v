1eb1835d250f87b52c7b76f17e10c1dc716e130c
OKU1987 <oku.87.s62.a4@gmail.com>
2015-02-20 21:00:32 +0900

    Define arity and variable_name

--- SIMT.v
+++ SIMT.v
@@ -5,4 +5,6 @@ Section SIMT_Definition.
   Variable num_threads : { n : nat & (n > 0)%nat}. (* the number of threads *)
   Definition N := projT1 num_threads.
 
+  Definition arity := nat.
+  Definition variable_name := nat.
 End SIMT_Definition.
