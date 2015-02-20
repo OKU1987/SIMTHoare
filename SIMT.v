8960b581a79becad5fa714e04cd3e2c82b52ee5f
OKU1987 <oku.87.s62.a4@gmail.com>
2015-02-20 21:03:01 +0900

    Define local variable and shared variable as inductive types

--- SIMT.v
+++ SIMT.v
@@ -7,4 +7,10 @@ Section SIMT_Definition.
 
   Definition arity := nat.
   Definition variable_name := nat.
+
+  Inductive LV (n:arity) : Set :=
+  | local : variable_name -> LV n.
+
+  Inductive SV (n:arity) : Set :=
+  | shared : variable_name -> SV n.
 End SIMT_Definition.
