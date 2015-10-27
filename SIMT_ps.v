c66c419d515b22be9a442f4bbbee5b9bfabc0df1
OKU1987 <oku.87.s62.a4@gmail.com>
2015-10-27 16:53:25 +0900

    Create a file to prove that prefix sum algorithms implemented in SIMT are correct.

---
+++ SIMT_ps.v
@@ -0,0 +1,7 @@
+Require Import SIMT_util.
+Require Import Vector_of_tuple.
+Require Import SIMT.
+Close Scope nat_scope.
+Require Import ssreflect ssrfun ssrbool eqtype ssrnat fintype.
+Require Import tuple div bigop finset ssralg ssrint intdiv.
+Require Import FunctionalExtensionality.
