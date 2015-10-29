3d3f4e71c8ab97f3147d35f450b04745555f65aa
OKU1987 <oku.87.s62.a4@gmail.com>
2015-10-29 15:51:00 +0900

    Delete tactic 'existT_eq'.
    It is no longer used, thanks to Ssreflect.

--- SIMT_util.v
+++ SIMT_util.v
@@ -1,36 +1,5 @@
-Require Import Vectors.VectorDef.
-Require Import ZArith.
-Import VectorNotations.
-Require Import JMeq.
 Require Import FunctionalExtensionality.
 Require Import Vector_of_tuple.
-
-Ltac existT_eq' :=
-  match goal with
-    | [ H : existT ?f ?t ?a = existT ?f ?t ?b,
-            H' : JMeq ?a ?b |- _ ] =>
-      subst; clear H
-    | [ H : existT ?f ?t ?a = existT ?f ?t ?b |- JMeq ?a ?b] =>
-      change (match existT f t a with
-                | existT t c => JMeq c b
-              end); rewrite H; constructor
-    | [ H : existT ?f ?t ?a = existT ?f ?t ?b |- _] =>
-      assert (JMeq a b)
-
-    | [ H : JMeq (existT ?f ?t ?a) (existT ?f ?t ?b),
-            H' : JMeq ?a ?b |- _ ] =>
-      subst; clear H
-    | [ H : JMeq (existT ?f ?t ?a) (existT ?f ?t ?b) |- JMeq ?a0 ?b0] =>
-      change (match existT f t a with
-                | existT t c => JMeq c b0
-              end); rewrite H; constructor
-    | [ H : JMeq (existT ?f ?t ?a) (existT ?f ?t ?b) |- _ ] =>
-      assert (JMeq a b)
-
-    | _ => fail
-  end.
-
-Ltac existT_eq := repeat existT_eq'.
 
 Ltac functional_extensionality_pair_l :=
   match goal with
